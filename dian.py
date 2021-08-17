c80f932293b0ec264af000695835cf20d2601e86
Roywaller <roywaller@126.com>
2021-08-17 13:25:59 +0800

    Update dian.py

--- dian.py
+++ dian.py
@@ -341,8 +341,9 @@ if setime != lastimeres:
 else:
     print("数据未更新")
 
-content = "用电情况\n\n房间号:" + RoomNum + "\n\n系统编号:" + SysNum + "\n\n累计表字:" + TotNum + "\n\n当前已用:" + str(
-    Used) + "\n\n购电剩余:" + LeftNum + "\n\n上次通讯时间:" + LastDate + "\n\n https://roywaller.github.io/XLFElectricity-Inquiry/Records.html" + "\n\n\n\n用水情况\n\n结算时间：" + setime + "\n\n累计读数：" + cumread + "\n\n结算水量：" + sewatervol + "\n\n剩余金额：" + balance
+content = str("用电情况\n\n房间号:" + RoomNum + "\n\n系统编号:" + SysNum + "\n\n累计表字:" + TotNum + "\n\n当前已用:" + str(Used) + \
+          "\n\n购电剩余:" + LeftNum + "\n\n上次通讯时间:" + LastDate + "\n\n https://roywaller.github.io/XLFElectricity-Inquiry/Records.html" + \
+          "\n\n\n\n用水情况\n\n结算时间：" + setime + "\n\n累计读数：" + cumread + "\n\n结算水量：" + sewatervol + "\n\n剩余金额：" + balance)
 
 api = "https://sctapi.ftqq.com/SCT62936TXA1lLED0i5z9u5qmYceyWavU.send"
 title = u"购电剩余：" + LeftNum + u"购水剩余：" + balance
